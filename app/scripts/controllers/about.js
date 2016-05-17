a0fa3cee0e77ad7146d5b8191b136b3c33aea7a4
Karl Reid <karlr42@gmail.com>
2016-05-17 20:43:59 +0100

    yeoman scaffold

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name karlreidieApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the karlreidieApp
+ */
+angular.module('karlreidieApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name karlreidieApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the karlreidieApp
+ */
+angular.module('karlreidieApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
