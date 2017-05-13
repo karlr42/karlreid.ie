dd46cea1d4312517ae8ae6fa2897db4c8c7d1397
Karl Reid <karlr42@gmail.com>
2017-05-13 20:18:35 +0100

    about->contact

---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name karlreidieApp.controller:ContactCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the karlreidieApp
+ */
+angular.module('karlreidieApp')
+  .controller('ContactCtrl', function () {
+    
+  });
